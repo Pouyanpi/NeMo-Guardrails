346bfda7c46ae89f034f007f717efad65f9f3748
Razvan Dinu <rdinu@nvidia.com>
2023-03-16 12:18:30 +0200

    Add basic setup.py for version 0.0.1.

---
+++ setup.py
@@ -0,0 +1,38 @@
+# Copyright (c) 2023, NVIDIA CORPORATION.  All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from setuptools import setup, find_packages
+
+setup(
+    name="collm",
+    version="0.0.1",
+    packages=find_packages(),
+    author="NVIDIA",
+    author_email="collm@nvidia.com",
+    description="CoLLM: Rails for LLMs.",
+    long_description="CoLLM is a framework for creating runtime rails for LLMs.",
+    long_description_content_type="text/markdown",
+    url="",
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+    ],
+)
