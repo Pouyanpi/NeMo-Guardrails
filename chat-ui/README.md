5b3ee94578e812ee5a20cc22bee2cc2d261f3a38
Razvan Dinu <rdinu@nvidia.com>
2023-04-04 11:50:37 +0300

    Add first version of the Chat UI frontend.

---
+++ chat-ui/README.md
@@ -0,0 +1,3 @@
+# Chat UI Frontend
+
+This folder includes a static build of the [Colang Flows Chat](https://gitlab-master.nvidia.com/dialogue-research/colangflows-ui) interface.
---
+++ chat-ui/frontend/_next/static/i-h6Xbq7jWjQ9HzUNol2Y/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/542b50fd-07ebedc579cef971.js","static/chunks/445-0dade6e6f572d658.js","static/chunks/pages/index-3f98a937ec9c91fd.js"],"/_error":["static/chunks/pages/_error-3f6d1c55bb8051ab.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
