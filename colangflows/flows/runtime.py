5aed42db755c06ed4e33d0baab8f5d324f30c991
Makesh Narsimhan Sreedhar <makeshn@nvidia.com>
2023-04-06 10:32:36 -0700

    uses register_action_params now

--- colangflows/flows/runtime.py
+++ colangflows/flows/runtime.py
@@ -6,6 +6,7 @@ from typing import List, Optional
 from colangflows.actions.actions import ActionResult
 from colangflows.actions.fact_checking import check_facts
 from colangflows.actions.math import wolfram_alpha_request
+from colangflows.actions.jailbreak_check import check_jailbreak
 from colangflows.flows.flows import FlowConfig, compute_context, compute_next_step
 from colangflows.rails.llm.config import RailsConfig
 
@@ -23,6 +24,7 @@ class Runtime:
         self.registered_actions = {
             "wolfram alpha request": wolfram_alpha_request,
             "check facts": check_facts,
+            "check jailbreak": check_jailbreak
         }
 
         # The list of additional parameters that can be passed to the actions.
@@ -213,9 +215,6 @@ class Runtime:
         if "context" in parameters:
             kwargs["context"] = context
         
-        if "llm" in parameters:
-            kwargs["llm"] = self.llm
-
         # Add any additional registered parameters
         for k, v in self.registered_action_params.items():
             if k in parameters:
