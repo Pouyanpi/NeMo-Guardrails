a52e3b8ac0d65d3769cd143161f2f74fe77e8df1
Razvan Dinu <rdinu@nvidia.com>
2023-04-07 11:34:54 +0300

    Small tweaks to fact checking. Migrate flows config to colang from yaml.

--- colangflows/actions/fact_checking.py
+++ colangflows/actions/fact_checking.py
@@ -1,5 +1,4 @@
 import logging
-import random
 from typing import Optional
 
 from langchain import LLMChain, PromptTemplate
@@ -9,19 +8,19 @@ log = logging.getLogger(__name__)
 
 
 async def check_facts(
-    query: Optional[str] = None,
     context: Optional[dict] = None,
     llm: Optional[BaseLLM] = None,
 ):
     """Checks the facts for the bot response."""
 
-    # TODO: fetch the relevant chunks
-    #  they should be in context["relevant_chunks"]
     evidence = context.get("relevant_chunks", [])
     bot_response = context.get("last_bot_message")
 
     if evidence:
-        fact_check_template = 'you are given a task to identify if the hypothesis is grounded and entailed to the evidence. you will only use the contents of the evidence and not rely on external knowledge. Answer with yes/no. "evidence": {evidence} "hypothesis": {response} "entails":'
+        fact_check_template = """
+            You are given a task to identify if the hypothesis is grounded and entailed to the evidence.
+            You will only use the contents of the evidence and not rely on external knowledge.
+            Answer with yes/no. "evidence": {evidence} "hypothesis": {response} "entails":"""
 
         prompt = PromptTemplate(
             template=fact_check_template, input_variables=["evidence", "response"]
@@ -33,6 +32,7 @@ async def check_facts(
         entails = entails.lower().strip()
         log.info(f"Entailment result is {entails}.")
 
-        if "yes" in entails:
-            return "The response is correct."
-    return "The response is unverified. Inform user you are not sure."
+        return "yes" in entails
+
+    # If there was no evidence, we always return true
+    return True
