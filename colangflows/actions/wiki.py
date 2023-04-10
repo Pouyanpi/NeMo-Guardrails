cfe62282cf81f34573837ea3261a900b795134f8
Pranjal Doshi <pranjald@nvidia.com>
2023-04-10 13:54:09 +0530

    Added wikipedia action from langchang with validation

---
+++ colangflows/actions/wiki.py
@@ -0,0 +1,41 @@
+import re
+from typing import Any, Dict
+
+from langchain.utilities.wikipedia import WikipediaAPIWrapper
+from langchain.utils import get_from_dict_or_env
+from pydantic import BaseModel, root_validator
+
+MAX_QUERY_LEN = 50
+
+
+class Wikipedia(WikipediaAPIWrapper):
+    query: str
+
+    @root_validator()
+    def validate_query(cls, values: Dict):
+        query = get_from_dict_or_env(values, "query", "")
+        if not isinstance(query, str) or not query:
+            raise ValueError("Query is not a valid string")
+        if len(query) > MAX_QUERY_LEN:
+            raise ValueError("Wikipedia Query length exceeded limits")
+
+        return values
+
+    def validate_response(self, response: str) -> str:
+        """Filter out IP addreess from the response."""
+
+        ip_regex = re.compile(r"\b(?:[0-9]{1,3}\.){3}[0-9]{1,3}\b")
+        response = re.sub(ip_regex, "", response)
+        return response
+
+    def run(self) -> str:
+        """Run query through Wikipedia and parse result."""
+
+        response = super().run(self.query)
+        return self.validate_response(response)
+
+
+# if __name__ == "__main__":
+#     wolfram_alpha_obj = Wikipedia(query="Who is the Joe Biden?")
+#     output = wolfram_alpha_obj.run()
+#     print(output)
