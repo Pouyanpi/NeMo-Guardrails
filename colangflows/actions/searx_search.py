079ae7db3a08195660da86ff5b7afc5166b039ff
Sagar Bogadi Manjunath <sbogadimanju@nvidia.com>
2023-04-10 09:53:17 +0530

    Add searchx search action

---
+++ colangflows/actions/searx_search.py
@@ -0,0 +1,52 @@
+import re
+from typing import Any, List, Optional
+
+from langchain.utilities.searx_search import SearxSearchWrapper
+from pydantic import validator
+
+MAX_QUERY_LEN = 5
+
+
+class SearxSearch(SearxSearchWrapper):
+    query: str
+    engines: Optional[List[str]] = []
+    categories: Optional[List[str]] = []
+    query_suffix: Optional[str] = ""
+
+    @validator("query")
+    def validate_query(cls, query: str):
+        if not isinstance(query, str) or not query:
+            raise ValueError("Query is not a valid string")
+        if len(query) > MAX_QUERY_LEN:
+            raise ValueError("Searx Search Query length exceeded limits")
+
+        return query
+
+    def validate_response(self, response: str) -> str:
+        """Filter out IP addreess from the response."""
+
+        ip_regex = re.compile(r"\b(?:[0-9]{1,3}\.){3}[0-9]{1,3}\b")
+        response = re.sub(ip_regex, "", response)
+        return response
+
+    def run(self, **kwargs: Any) -> str:
+        """Run query through Searx Search and parse result."""
+
+        response = super().run(
+            self.query, self.engines, self.categories, self.query_suffix, **kwargs
+        )
+        return self.validate_response(response)
+
+    async def arun(self, **kwargs: Any) -> str:
+        """Call async run method of Searx Search langchain tool."""
+
+        response = await super().arun(
+            self.query, self.engines, self.query_suffix, **kwargs
+        )
+        return self.validate_response(response)
+
+
+# if __name__ == "__main__":
+#     searx_obj = SearxSearch(query="Who is the Joe Biden?")
+#     output = searx_obj.run()
+#     print(output)
