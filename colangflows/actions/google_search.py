3dd5e69810e221783722376f6b823b87b99058bb
Sagar Bogadi Manjunath <sbogadimanju@nvidia.com>
2023-04-10 10:25:36 +0530

    Add Google Search action

---
+++ colangflows/actions/google_search.py
@@ -0,0 +1,38 @@
+import re
+
+from langchain.utilities.google_search import GoogleSearchAPIWrapper
+from pydantic import validator
+
+MAX_QUERY_LEN = 50
+
+
+class GoogleSearch(GoogleSearchAPIWrapper):
+    query: str
+
+    @validator("query")
+    def validate_query(cls, query: str):
+        if not isinstance(query, str) or not query:
+            raise ValueError("Query is not a valid string")
+        if len(query) > MAX_QUERY_LEN:
+            raise ValueError("Google Search Query length exceeded limits")
+
+        return query
+
+    def validate_response(self, response: str) -> str:
+        """Filter out IP addreess from the response."""
+
+        ip_regex = re.compile(r"\b(?:[0-9]{1,3}\.){3}[0-9]{1,3}\b")
+        response = re.sub(ip_regex, "", response)
+        return response
+
+    def run(self) -> str:
+        """Run query through Google Search and parse result."""
+
+        response = super().run(self.query)
+        return self.validate_response(response)
+
+
+# if __name__ == "__main__":
+#     serp_obj = GoogleSearch(query="Who is the Joe Biden?")
+#     output = serp_obj.run()
+#     print(output)
