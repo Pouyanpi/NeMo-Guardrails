2709552d7908061b57307f00d66b161eee962815
Traian Rebedea <trebedea@nvidia.com>
2023-04-14 12:54:05 +0300

    Deleted utils.py from hallucination rail,

--- colangflows/actions/hallucination/utils.py
+++
@@ -1,6 +0,0 @@
-import bert_score
-from typing import List
-
-
-def compute_score(self, response: str, other_responses: List[str]):
-    P, R, F1 = bert_score.score([response], other_responses, lang="en", model_type="facebook/bart-base", verbose=False)
