9aabc2e657b1ed7160312a7f35ebb0dde6c993cf
Pouyan <prezakhanipr@gmail.com>
2023-06-01 11:19:08 +0000

    Implement registered and splitter registry
    Signed-off-by: Pouyan <prezakhanipr@gmail.com>

---
+++ nemoguardrails/kb/splitters/registered.py
@@ -0,0 +1,173 @@
+# SPDX-FileCopyrightText: Copyright (c) 2023 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+# SPDX-License-Identifier: Apache-2.0
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import contextlib
+import inspect
+from abc import ABC, abstractmethod
+from typing import Any, ClassVar, Iterator, List
+
+from nemoguardrails.kb import utils
+
+from . import SPLITTERS
+
+_SPLITTER_MODULE_SUFFIX = "_splitter"
+_SPLITTER_REGISTRY = SPLITTERS
+_ABSTRACT_SPLITTER_REGISTRY = {}
+
+_skip_registration = False
+
+
+class RegisteredSplitter(ABC):
+    """Subclasses will be registered and given a `name` property."""
+
+    # Name of the splitter, automatically filled if not already set.
+    name: ClassVar[str]
+
+    def __init_subclass__(
+        cls, skip_registration=False, **kwargs
+    ):  # pylint: disable=redefined-outer-name
+        super().__init_subclass__(**kwargs)
+
+        # Set the name if the splitter does not define it.
+        # Use __dict__ rather than getattr so subclasses are not affected.
+        if not cls.__dict__.get("name"):
+            if cls.__name__ == "Splitter":
+                # Config-based splitters should be defined with a class named "Splitter".
+                # In such a case, the splitter name is extracted from the module if it
+                # follows conventions:
+                module_name = cls.__module__.rsplit(".", 1)[-1]
+                if module_name.endswith(_SPLITTER_MODULE_SUFFIX):
+                    cls.name = module_name[: -len(_SPLITTER_MODULE_SUFFIX)]
+                elif "." in cls.__module__:  # Extract splitter name from package name.
+                    cls.name = cls.__module__.rsplit(".", 2)[-2]
+                else:
+                    raise AssertionError(
+                        "When using `Splitter` as class name, the document splitter name is "
+                        'inferred from module name if named "*_splitter" or from '
+                        f'package name, but there is no package in "{cls.__module__}".'
+                    )
+            else:
+                cls.name = utils.camelcase_to_snakecase(cls.__name__)
+
+        is_abstract = inspect.isabstract(cls)
+
+        # Skip splitter registration within contextmanager, or if skip_registration
+        # is passed as meta argument.
+        if skip_registration or _skip_registration:
+            return
+
+        # Check for name collisions
+        if cls.name in _SPLITTER_REGISTRY:
+            raise ValueError(f"Splitter with name {cls.name} already registered.")
+        elif cls.name in _ABSTRACT_SPLITTER_REGISTRY:
+            raise ValueError(
+                f"Splitter with name {cls.name} already registered as abstract."
+            )
+
+        # Add the splitter to the registers
+        if is_abstract:
+            _ABSTRACT_SPLITTER_REGISTRY[cls.name] = cls
+        else:
+            _SPLITTER_REGISTRY[cls.name] = cls
+
+
+def refresh_splitters(func):
+    def wrapper(self, *args, **kwargs):
+        self._refresh()
+        return func(self, *args, **kwargs)
+
+    return wrapper
+
+
+class SplitterRegistry:
+    def __init__(self) -> None:
+        self.splitters = _SPLITTER_REGISTRY
+        # self.validate_splitters()
+
+    def _refresh(self):
+        self.splitters = _SPLITTER_REGISTRY
+
+    @refresh_splitters
+    def __get__(self, class_name: str):
+        return self.get(class_name)
+
+    @refresh_splitters
+    def get(self, class_name):
+        """
+        Get a splitter by name.
+
+        :param class_name: The name of the splitter.
+        :raises KeyError: If the splitter name does not exist in the registry.
+        """
+        if class_name is None:
+            raise ValueError("class_name cannot be None")
+
+        if class_name not in self.splitters:
+            raise KeyError(f"{class_name} does not exist in the registry")
+
+        return self.splitters.get(class_name)
+
+    @refresh_splitters
+    def validate_splitters(self) -> None:
+        """
+        Validate that all splitters have a 'split' method.
+
+        :raises ValueError: If a splitter does not have a 'split' method.
+        """
+        for splitter_name, splitter in self.splitters.items():
+            if not hasattr(splitter, "split_text") or not callable(splitter.split_text):
+                raise ValueError(
+                    f"{splitter_name} and thus {splitter} does not have a 'split_text' method"
+                )
+
+    @refresh_splitters
+    def list(self) -> List[str]:
+        """
+        List all splitters in the registry.
+
+        :return: A list of splitter names.
+        """
+        return list(self.splitters.keys())
+
+    @refresh_splitters
+    def __repr__(self) -> str:
+        return f"SplitterRegistry(splitters={self.splitters})"
+
+    @refresh_splitters
+    def __len__(self) -> int:
+        return len(self.splitters)
+
+    @refresh_splitters
+    def __iter__(self) -> Iterator[str]:
+        return iter(self.splitters)
+
+    @refresh_splitters
+    def __contains__(self, splitter_name: str) -> bool:
+        return splitter_name in self.splitters
+
+    @refresh_splitters
+    def __getitem__(self, splitter_name: str) -> Any:
+        return self.get(splitter_name)
+
+
+@contextlib.contextmanager
+def skip_registration() -> Iterator[None]:
+    """Context manager within which dataset builders are not registered."""
+    global _skip_registration
+    try:
+        _skip_registration = True
+        yield
+    finally:
+        _skip_registration = False
